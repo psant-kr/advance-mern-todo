1bf2f6b5ab1ea383a03bdb2bc7240a15ec5a03eb
prashant kumar <prashantdoa1996@gmail.com>
2023-09-15 17:42:24 +0530

    redux store initialized

---
+++ client/src/redux/store.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({
+    todos: todosReducers
+})
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store;
